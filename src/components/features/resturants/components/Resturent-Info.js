5492412d75678fc5646b67f5e49bd5de119aaf58
Ahsan-Baig <ahsanhuraira99@gmail.com>
2023-11-22 12:58:25 +0500

    card component of returent info completed

--- src/components/features/resturants/components/Resturent-Info.js
+++ src/components/features/resturants/components/Resturent-Info.js
@@ -1,6 +1,8 @@
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable prettier/prettier */
 import * as React from 'react';
-import {StyleSheet, View} from 'react-native';
-import {Card, Text} from 'react-native-paper';
+import {Image, StyleSheet, View, Text} from 'react-native';
+import {Card} from 'react-native-paper';
 import {colors} from '../../../../theme/colors';
 import {space} from '../../../../theme/spacings';
 import {fontSizes} from '../../../../theme/fonts';
@@ -11,15 +13,14 @@ import open from '../../../../../assets/open';
 export const ResturentInfo = ({Resturants = {}}) => {
   const {
     name = 'KFC',
-    icon,
+    icon = 'https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png',
     photos = ['https://picsum.photos/700'],
     address = '23rd street downhill',
-    isOpen = true,
-    ratings = 3.2,
-    IsClosedTemporarily,
+    isOpen = false,
+    ratings = 5,
+    IsClosedTemporarily = true,
   } = Resturants;
   const ratingArray = Array.from(new Array(Math.floor(ratings)));
-  console.log(ratingArray);
   return (
     <Card style={{backgroundColor: colors.bg.primary}}>
       <Card.Cover
@@ -29,15 +30,23 @@ export const ResturentInfo = ({Resturants = {}}) => {
       />
       <Card.Content>
         <Text style={styles.title}>{name}</Text>
-        <View style={styles.svg}>
+        <View style={styles.cardItems}>
           <View style={styles.rating}>
-            {ratingArray.map(() => (
-              <SvgXml xml={star} style={{height: 20, width: 20}} />
+            {ratingArray.map(b => (
+              <SvgXml key={b} xml={star} style={{height: 20, width: 20}} />
             ))}
           </View>
-          <SvgXml xml={open} style={{height: 20, width: 20}} />
+          {IsClosedTemporarily && !isOpen && (
+            <Text style={{color: colors.ui.error}}>Closed Temporarily</Text>
+          )}
+          {isOpen && !IsClosedTemporarily && (
+            <SvgXml xml={open} style={{height: 20, width: 20}} />
+          )}
         </View>
-        <Text style={styles.text}>{address}</Text>
+        <View style={styles.cardItems}>
+          <Text style={styles.text}>{address}</Text>
+          <Image src={icon} style={{height: 20, width: 20, flexWrap: 'wrap'}} />
+        </View>
       </Card.Content>
     </Card>
   );
@@ -47,6 +56,7 @@ const styles = StyleSheet.create({
     fontFamily: 'Lato-Italic',
     fontSize: fontSizes.body,
     color: colors.ui.primary,
+    flexWrap: 'wrap',
   },
   text: {
     fontFamily: 'Lato-Italic',
@@ -57,8 +67,9 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     paddingVertical: 2,
   },
-  svg: {
+  cardItems: {
     flexDirection: 'row',
     justifyContent: 'space-between',
+    flexWrap: 'wrap',
   },
 });
